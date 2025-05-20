d1144cb8725595c74e1474ff662745e0bc0d385b
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-20 15:18:58 +0200

    feat(routes): add routes for authors and books management
    Added RESTful routes for managing authors and books, including listing, creating, updating, and deleting. Also introduced a specific route for toggling the borrowed status of books.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,25 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Author routes
+    Route::prefix('authors')->group(function () {
+        Route::get('/', [App\Http\Controllers\AuthorController::class, 'index'])->name('authors.index');
+        Route::post('/', [App\Http\Controllers\AuthorController::class, 'store'])->name('authors.store');
+        Route::get('/{id}', [App\Http\Controllers\AuthorController::class, 'show'])->name('authors.show');
+        Route::put('/{id}', [App\Http\Controllers\AuthorController::class, 'update'])->name('authors.update');
+        Route::delete('/{id}', [App\Http\Controllers\AuthorController::class, 'destroy'])->name('authors.destroy');
+    });
+
+    // Book routes
+    Route::prefix('books')->group(function () {
+        Route::get('/', [App\Http\Controllers\BookController::class, 'index'])->name('books.index');
+        Route::post('/', [App\Http\Controllers\BookController::class, 'store'])->name('books.store');
+        Route::get('/{id}', [App\Http\Controllers\BookController::class, 'show'])->name('books.show');
+        Route::put('/{id}', [App\Http\Controllers\BookController::class, 'update'])->name('books.update');
+        Route::delete('/{id}', [App\Http\Controllers\BookController::class, 'destroy'])->name('books.destroy');
+        Route::patch('/{id}/toggle-borrowed', [App\Http\Controllers\BookController::class, 'toggleBorrowedStatus'])->name('books.toggle-borrowed');
+    });
 });
 
 require __DIR__.'/auth.php';
