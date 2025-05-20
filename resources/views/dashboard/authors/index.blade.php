07a516dd3a080eccfa9e9fcf05b4ae113d972bc0
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-20 16:32:03 +0200

    feat(components): add dynamic URLs for API interactions
    Updated AuthorList and BookList components to use dynamic URLs passed as props for API interactions, improving flexibility and reusability. Adjusted corresponding Blade templates to provide these URLs consistently.

--- resources/views/dashboard/authors/index.blade.php
+++ resources/views/dashboard/authors/index.blade.php
@@ -4,6 +4,12 @@
 
 @section('content')
 <div id="app">
-    <author-list></author-list>
+    <author-list
+        :index-url="'{{ route('api.authors.index') }}'"
+        :store-url="'{{ route('authors.store') }}'"
+        :show-url="'{{ route('authors.show', ['id' => '__id__']) }}'"
+        :update-url="'{{ route('authors.update', ['id' => '__id__']) }}'"
+        :delete-url="'{{ route('authors.destroy', ['id' => '__id__']) }}'"
+    ></author-list>
 </div>
 @endsection
