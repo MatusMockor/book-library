85d9ba6c93a4c7e55231515bf06a53b205db8dfe
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-20 15:05:52 +0200

    pint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\AuthorRepository;
+use App\Repositories\BookRepository;
+use App\Repositories\Interfaces\AuthorRepository as AuthorRepositoryContract;
+use App\Repositories\Interfaces\BookRepository as BookRepositoryContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(AuthorRepositoryContract::class, AuthorRepository::class);
+        $this->app->bind(BookRepositoryContract::class, BookRepository::class);
     }
 
     /**
