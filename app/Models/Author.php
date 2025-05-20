a3b443e3cddd5c82786c5810a40be5eeb0b08f95
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-20 15:10:22 +0200

    feat(models): add return type to Author's books method
    Added the HasMany return type to the books method in the Author model. This improves type hinting and enhances code clarity for the relationship definition.

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Author extends Model
 {
@@ -14,7 +15,7 @@ class Author extends Model
         'surname',
     ];
 
-    public function books()
+    public function books(): HasMany
     {
         return $this->hasMany(Book::class);
     }
