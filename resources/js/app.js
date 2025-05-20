0addb62eac2e35bd21848f7f208ecad4903479c1
Matúš Močkor <33400567+MatusMockor@users.noreply.github.com>
2025-05-20 15:02:54 +0200

    Remove unused ExampleComponent registration in app.js
    The ExampleComponent import and registration were removed as they are no longer being used. This cleanup helps reduce unnecessary code and improves maintainability.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import ExampleComponent from './components/ExampleComponent.vue';
 
 
 // Initialize Vue
 const app = createApp({});
-app.component('example-component', ExampleComponent);
 app.mount('#app');
